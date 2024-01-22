e4f0e86f8b858a97c32fdec2723aca964c8f0632
Ganga Ram Mahato <grmmahato12@gmail.com>
2024-01-22 16:29:11 +0545

    add contact page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:manoj_dai/presentation/screens/contact_us.dart';
 
 import 'presentation/screens/language_screen.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LanguageScreen(),
+      home: const ContactUs(),
     );
   }
 }
